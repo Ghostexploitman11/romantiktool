ddee64f4c441ab6be37c12f013ca66af10c0870a
Ghostexploitman11 <hayalet001@proton.me>
2025-02-10 15:07:33 -0500

    Add files via upload

---
+++ romantiktool.py
@@ -0,0 +1,12 @@
+isim = input("İsminizi rica etsem saygı değer hanım efendi?: ")
+
+print("Günün sözüne hazırmısınız" + " " + isim + "hanım hazırmısın hazırsan enter de")
+
+
+hazır =input("hazırsan evet de: ")	
+
+
+print(isim + " hanım şu hayatı serverimde tek vatanım olup benimle çıkarmısın")
+
+
+
